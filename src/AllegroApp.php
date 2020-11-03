90d434b0260198e8f6365bd98d15d188d943e4d0
Antanas Kompanas <a.kompanas@gofintech.lt>
2020-11-03 19:43:31 +0200

    Storing  AllegroApp instance in the container.

--- src/AllegroApp.php
+++ src/AllegroApp.php
@@ -52,6 +52,7 @@ class AllegroApp
             $this->appDir = $this->findApplicationDir();
             $this->configLocator = new FileLocator(["{$this->appDir}/config", $this->appDir]);
             $this->container = $this->loadServiceDefinitions($this->configLocator);
+            $this->container->set(AllegroApp::class, $this);
         }
         catch (RuntimeException $ex) {
             throw $ex;
